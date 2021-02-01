e050389515597801983fbe4405db687c03c6ce6d
Edward Crawford <erac915@icloud.com>
2021-02-01 15:30:34 -0500

    step 4

--- js/basics.js
+++ js/basics.js
@@ -2,3 +2,12 @@ const daysPerYear = 365;
 console.log(daysPerYear);
 const hoursPerDay=24
 console.log(hoursPerDay)
+const minutesPerHour=60
+console.log(minutesPerHour)
+const hoursPerYear = hoursPerDay * daysPerYear
+console.log(hoursPerYear)
+const minutesPerDay = minutesPerHour * hoursPerDay
+console.log(minutesPerDay)
+
+
+
